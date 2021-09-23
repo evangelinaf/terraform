5fa33f06bab64bd304f0d120ae35185b0ce54a40
efiorucci <efiorucci@whitepages.com>
2021-09-23 13:42:04 -0300

    Adding new changes

--- main.tf
+++ main.tf
@@ -7,16 +7,107 @@ terraform {
   }
 }
 
+data "aws_availability_zones" "all" {}
+
 provider "aws" {
   profile = "efiorucci"
   region  = "us-west-2"
 }
 
-resource "aws_instance" "ec2-instance" {
-  ami           = "ami-22eb9e5a"
+resource "aws_launch_configuration" "launch_configuration" {
+  image_id  = "ami-22eb9e5a"
   instance_type = "t1.micro"
+  security_groups = ["${aws_security_group.security-group.id}"]
 
-  tags = {
-    Name = "Terraform-test-by-Eva"
-    }
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+          
+  lifecycle {
+    create_before_destroy = true
   }
+}
+
+
+resource "aws_security_group" "security-group" {
+  name = "terraform-segurity-group"
+
+  ingress {
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "asg" {
+  launch_configuration = "${aws_launch_configuration.launch_configuration.id}"
+  #availability_zones = ["${data.aws_availability_zones.all.names}"]
+  availability_zones = data.aws_availability_zones.all.names
+
+  load_balancers = ["${aws_elb.elb.name}"]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_security_group" "sg_elb" {
+  name = "terraform-example-sg"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_elb" "elb" {
+  name = "terraform-elb-example"
+  #availability_zones = ["${data.aws_availability_zones.all.names}"]
+  availability_zones = data.aws_availability_zones.all.names
+  security_groups = ["${aws_security_group.sg_elb.id}"]
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+}
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.elb.dns_name}"
+}
